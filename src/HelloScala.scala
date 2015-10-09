1825394b26dde2bb2f9f001e5d863b82c96fb68b
qiqi77 <lotusws7@163.com>
2015-10-09 23:18:24 +0800

    1

---
+++ src/HelloScala.scala
@@ -0,0 +1,35 @@
+package exercise.sqlPaper
+
+/**
+ * Created by lenovo on 2015/10/9.
+ */
+object HelloScala {
+  def main(args: Array[String]): Unit ={
+    println("Hello Scala!! A new World")
+    for(arg <- args) println(arg)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
