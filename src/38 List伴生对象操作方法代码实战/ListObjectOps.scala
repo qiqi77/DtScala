258730e9af6ddb692f6bcbf17585990fe6ffdea7
qiqi77 <lotusws7@163.com>
2015-10-29 22:28:07 +0800

    38

---
+++ src/38 List伴生对象操作方法代码实战/ListObjectOps.scala
@@ -0,0 +1,23 @@
+package dt.scala
+
+/**
+ * Created by lenovo on 2015/10/29.
+ */
+object ListObjectOps {
+  def main(args: Array[String]): Unit ={
+    println(List.apply(1, 2, 3))
+    println(List.make(3, 5))
+    println(List.range(1, 5))
+    println(List.range(9, 1, -3))
+
+    val zipped = "abcde".toList zip List(1, 2, 3, 4, 5)
+    println(zipped)
+    println(zipped.unzip)
+
+    println(List(List('a', 'b'), List('c'), List('d', 'e')).flatten)
+    println(List.concat(List(), List('b'), List('e')))
+
+    println(List.map2(List(10, 20), List(10, 10)) (_ * _))
+
+  }
+}
